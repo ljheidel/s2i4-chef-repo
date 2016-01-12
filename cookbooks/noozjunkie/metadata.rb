79447466c50cc33bfcca3521f131588ff8b82f1b
Luke Heidelberger <ljheidel@gmail.com>
2016-01-11 22:20:10 -0500

    Added noozjunkie cookbook

---
+++ cookbooks/noozjunkie/metadata.rb
@@ -0,0 +1,7 @@
+name             'noozjunkie'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures noozjunkie'
+long_description 'Installs/Configures noozjunkie'
+version          '0.1.0'
